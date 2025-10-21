8ab6e0f6162eca73f03aa6d68afbe09d997ba64c
Im47401 <im47401@mail.harpercollege.edu>
2025-10-21 16:40:16 +0000

    submitting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-#include "src/prime.hpp"
+static string normalize_token(const string &s) {
+  string out;
+  out.reserve(s.size())
+  for (unsigned char c : s) {
+     if (isalnum(c)) out.push_back((char)tolower(c));
+  }
+  return out;
+}
 
-int main() {
-  // Your driver program goes here
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cerr << "Usage: " << argv[0] << " <input.txt>\n";
+    return 2;
+  }
+  ifstream in(argv[1];
+    if (!in) {
+      cerr << "Failed to open file: " << argv[1] << "\n"
+      return 3;
+    }
+
+  unodered_map<string, set<int>> index;
+  string line;
+  int lineno = 0;
+  
 }
